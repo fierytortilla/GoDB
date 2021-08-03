3168a70db0609572a6a88d1ee0bcb1168ae1561b
David <dschlessinger@live.com>
2021-08-03 14:57:42 +0100

    modified the load_samples.sh script so that it automatically adds in any vcf.gz file present in the respective assaytype folder with a chromosome number between 1 and 22.

--- load/sh/load_samples.sh
+++ load/sh/load_samples.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export CONFFILE=$1
 source ${CONFFILE}
-tabix -h ${DBDATADIR}/chr19.vcf.gz 0:0-0 | python ${PYLDIR}/load_samples.py --assaytype=${ASSAYTYPE}
-
+#tabix -h ${DBDATADIR}/chr19.vcf.gz 0:0-0 | python ${PYLDIR}/load_samples.py --assaytype=${ASSAYTYPE}
+for i in $(seq 1 22); do if [ -e ${DBDATADIR}/chr${i}.vcf.gz ]; then tabix -h ${DBDATADIR}/chr${i}.vcf.gz 0:0-0 | python ${PYLDIR}/load_samples.py --assaytype=${ASSAYTYPE}; fi; done
